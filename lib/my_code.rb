a95c793f7bde08635bc83eaa89d68aed89884962
Melike KILIC <melike180400@gmail.com>
2019-10-26 00:33:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,4 +10,16 @@ def map(array)
 end
 
 def reduce(array,s_point=nil)
+  if s_point
+    sum=s_point
+    i=0
+  else
+    sum= array[0]
+   i=1
+  end
+  while i< array.length
+    sum= yield(sum,array[i])
+    i+=1
+  end
+  sum
 end
